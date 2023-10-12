50cba96caf8f40ca0a2c5e916e6228cd1b3c5652
camilla alves <camillaalves285@gmail.com>
2023-10-12 13:09:24 -0300

    Adicionando o projeto

---
+++ src/components/Thumbnail/Thumbnail.tsx
@@ -0,0 +1,22 @@
+import { ReactComponent as NoThumbnailIcon }  from '../../icons/photo.svg'
+import { Container } from './Thumbnail.styles'
+
+interface ThumbnailProps {
+   thumbnail?: string
+   title: string
+   size?: 'small' | 'large'
+   bgColor: string
+}
+
+export function Thumbnail({bgColor, thumbnail, title, size ='small'}: ThumbnailProps) {
+   return (
+      <Container bgColor={bgColor} size={size}>
+         {thumbnail? (
+            <img src={thumbnail} alt={title} />
+         ) : (
+            <NoThumbnailIcon/>
+         )}
+
+      </Container>
+   )
+}
